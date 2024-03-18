a72cbbbf2200f38a90478e823c37f3025d4fe508
Ankita <ankita7saha@gmail.com>
2024-03-18 11:56:25 +0600

    this is the 5th problem

--- src/LetterSizePaper.java
+++ src/LetterSizePaper.java
@@ -1,25 +1,16 @@
 import java.util.Scanner;
 public class LetterSizePaper {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter 1st value: ");
-        int first = sc.nextInt();
-        System.out.print("Please enter 2nd value: ");
-        int second = sc.nextInt();
-        double sum = (first + second);
-        double difference = (first - second);
-        double product = (first * second);
-        double average = (first + second)/2;
-        double distance = Math.abs(difference);
-        double max = Math.max(first, second);
-        double min = Math.min(first, second);
-        System.out.println("Summation:    " + sum);
-        System.out.println("Difference:   " + difference);
-        System.out.println("Product:      " + product);
-        System.out.println("Average:      " + average);
-        System.out.println("Distance:     " + distance);
-        System.out.println("Maximum:      " + max);
-        System.out.println("Minimum:      " + min);
+        System.out.print("Enter a distance in meters: ");
+        double meters = sc.nextDouble();
+        double miles = meters * 0.000621371;
+        double feet = meters * 3.28084;
+        double inches = meters * 39.37008;
+        System.out.printf("%.5f meters is equal to:\n", meters);
+        System.out.printf("%.5f miles\n", miles);
+        System.out.printf("%.5f feet\n", feet);
+        System.out.printf("%.5f inches\n", inches);
     }
 }
 
