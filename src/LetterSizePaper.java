75814ca0025e1325bc4c8d887f9edbbfd632e45a
Ankita <ankita7saha@gmail.com>
2024-03-18 12:01:01 +0600

    this is the 6th problem

--- src/LetterSizePaper.java
+++ src/LetterSizePaper.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class LetterSizePaper {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a distance in meters: ");
-        double meters = sc.nextDouble();
-        double miles = meters * 0.000621371;
-        double feet = meters * 3.28084;
-        double inches = meters * 39.37008;
-        System.out.printf("%.5f meters is equal to:\n", meters);
-        System.out.printf("%.5f miles\n", miles);
-        System.out.printf("%.5f feet\n", feet);
-        System.out.printf("%.5f inches\n", inches);
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+        double area = 3.14 * Math.pow(radius, 2);
+        double circumference = 2 * 3.14 * radius;
+        double volume = (4.0 / 3.0) * 3.14 * Math.pow(radius, 3);
+        double surfaceArea = 4 * 3.14 * Math.pow(radius, 2);
+        System.out.printf("Area of circle: %.2f \n", area);
+        System.out.printf("Circumference of circle: %.2f \n", circumference);
+        System.out.printf("Volume of Sphere : %.2f \n", volume);
+        System.out.printf("Surface Area of Sphere: %.2f \n", surfaceArea);
     }
 }
-
